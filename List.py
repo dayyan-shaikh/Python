e8c3b1fec21dcf31769217d947210faebdf276b9
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-08-18 18:00:11 +0530

    commit

---
+++ List.py
@@ -0,0 +1,13 @@
+color=["red","yellow","green","Blue","voilet"]
+for i in color:
+    if (i=="yellow"):
+        print("Yellow is here")
+    else:
+        ("Not here")
+
+
+name=["dayyan","sumit","ayan","abhishek","sai"]
+new=[item for item in name if (len(item)>4)]
+print(new)
+
+
