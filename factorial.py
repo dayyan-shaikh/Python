369f49844f6838a44826742981ba0fded6a82162
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2023-08-23 19:10:23 +0530

    commit

--- factorial.py
+++ factorial.py
@@ -1,5 +1,14 @@
 #factorial of number using math module
-import math
-x=int(input("Enter number: "))
-b=math.factorial(x)
-print(b)
+# import math
+# x=int(input("Enter number: "))
+# b=math.factorial(x)
+# print(b)
+
+
+#factorial of number using recursion method
+# def factorial(n):
+#     if (n==0) or (n==1):
+#         return 1
+#     else:
+#         return(n*factorial(n-1))
+# print(factorial(5))
